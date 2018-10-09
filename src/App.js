300d82db3a13d769231d3a477b9b019d82ee484f
Breena Troy <breena17@gmail.com>
2018-10-08 22:55:30 -0700

    map shows but might not be installed right

--- src/App.js
+++ src/App.js
@@ -10,11 +10,39 @@ class App extends Component {
     }
     return (
       <div>
-        <nav id="top-nav" style={{backgroundImage: 'url(' + require('./img/beachfinder-nav.jpg') + ')'}}>
+        <nav id="top-nav">
           <h1>BeachFinder</h1>
         </nav>
         <div id="map">
-        <Map google={this.props.google} />
+        <Map 
+          google={this.props.google} 
+          zoom={11}
+          style={{height: 'calc(100% - 101px)'}}
+          initialCenter={{
+            lat: 47.638459,
+            lng: -122.367408
+          }}>
+        <Marker onClick={this.onMarkerClick}
+                position={{lat:47.6918 , lng:-122.4031}}
+                name={'Golden Gardens'} />
+        <Marker onClick={this.onMarkerClick}
+                position={{lat:47.6608683, lng:-122.4328379}}
+                name={'Discovery Park Beach'}/>
+        <Marker onClick={this.onMarkerClick}
+                position={{lat:47.5791, lng:-122.4114}}
+                name={'Alki Beach'}/>
+        <Marker onClick={this.onMarkerClick}
+                position={{lat:47.7048, lng:-122.2154}}
+                name={'Juanita Beach Park'}/>
+        <Marker onClick={this.onMarkerClick}
+                position={{lat:47.6194, lng:-122.3618}}
+                name={'Myrtle Edwards Park'}/>
+        <InfoWindow onClose={this.onInfoWindowClose}>
+            <div>
+              <h1>Marker</h1>
+            </div>
+        </InfoWindow>
+        </Map>
         </div>
       </div>  
     );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,36 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-class Map extends React.Component {
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.google !== this.props.google) {
-            this.loadMap();
-        }
+
+class Map extends Component {
+  render() {
     }
-    loadMap() {
-        if (this.props && this.props.google) {
-            const {google} = this.props;
-            const maps = google.maps;
+    return () {
 
-            const mapRef = this.refs.map;
-            const node = ReactDOM.findDOMNode(mapRef);
-
-            let zoom = 12;
-            let lat = 47.638459;
-            let lng = -122.367408;
-            const center = new maps.LatLng(lat, lng);
-            const mapConfig = Object.assign({}, {
-                center: center,
-                zoom: zoom
-            })
-            this.map = new maps.Map(node, mapConfig);
-        }
-    }
-    render() {
-        return (
-            <div ref='map'>
-            Loading map...
-            </div>
-        )
     }
 }
+
 export default Map
