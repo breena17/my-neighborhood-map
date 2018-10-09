a952cb2bad98c005feb0f44dea4b44b9bc62505b
Breena Troy <breena17@gmail.com>
2018-10-08 17:40:26 -0700

    map showing, nav bar created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import Map from './components/Map'
 import './App.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 class App extends Component {
   render() {
+    if (!this.props.loaded) {
+      return <div>Loading...</div>
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>
+        <nav id="top-nav" style={{backgroundImage: 'url(' + require('./img/beachfinder-nav.jpg') + ')'}}>
+          <h1>BeachFinder</h1>
+        </nav>
+        <div id="map">
+        <Map google={this.props.google} />
+        </div>
+      </div>  
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyCjl6vYjUvbWFn9KPvdPdgea0umn1B8d-4")
+})(App)
