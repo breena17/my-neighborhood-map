16aaf7ca0c7a6ace8f98ab6adab10cce052215a2
Breena Troy <breena17@gmail.com>
2018-10-18 23:00:33 -0700

    working on dynamic markers

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -23,14 +23,17 @@ class MapContainer extends Component {
           console.log(Response.data.response.groups[0].items)
           this.setState({
             venues: Response.data.response.groups[0].items
-          }, this.getMarkers())
+          }, () => {
+            this.setState({ load: false})
+          } )
         })
         .catch(error => {
             console.log(error)
         })
     }
-    
+    /*
     getMarkers = () => {
+      
       this.state.venues.map(myVenue => {
           let marker = new window.google.maps.Marker({
             position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
@@ -40,37 +43,49 @@ class MapContainer extends Component {
           })
           this.markers.push(marker)
       })
-    }
+    }*/
     
   render() {
     if (!this.props.loaded) {
       return <div>Loading...</div>
     }
+    console.log(this.state.venues)
+    this.state.venues.map(myVenue => {
+      let marker = new window.google.maps.Marker({
+        position: {lat: myVenue.venue.location.lat, lng: myVenue.venue.location.lng},
+        map: Map,
+        title: myVenue.venue.name,
+        id: myVenue.venue.id 
+      })
+      this.state.markers.push(marker)
+      console.log(marker)
+    })
     
+  
     return (
         <Map 
           google={this.props.google} 
-          zoom={11}
+          zoom={10}
           style={{height: 'calc(100% - 101px)'}}
           initialCenter={{
-            lat: 47.638459,
-            lng: -122.367408
+            lat: 47.6194,
+            lng: -122.6031
           }}>
         <Marker onClick={this.onMarkerClick}
-                position={{lat: 47, lng: -122}}
+                position={{lat: 47.6918, lng: -122.4031}}
                 title={this.state.markers.title}/>
         <Marker onClick={this.onMarkerClick}
                 position={{lat:47.6608683, lng:-122.4328379}}
                 title={this.state.markers.title}/>
         <Marker onClick={this.onMarkerClick}
                 position={{lat:47.5791, lng:-122.4114}}
-                name={'Alki Beach'}/>
+                title={'Alki Beach'}/>
         <Marker onClick={this.onMarkerClick}
                 position={{lat:47.7048, lng:-122.2154}}
-                name={'Juanita Beach Park'}/>
+                title={'Juanita Beach Park'}/>
         <Marker onClick={this.onMarkerClick}
                 position={{lat:47.6194, lng:-122.3618}}
-                name={'Myrtle Edwards Park'}/>
+                title={'Myrtle Edwards Park'}/>
         <InfoWindow onClose={this.onInfoWindowClose}>
             <div>
               <h1>Marker</h1>
